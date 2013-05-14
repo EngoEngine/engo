d8fcbe48fc19525b5ac0ac1271f36b5e52e57e8a
Joseph Hager <ajhager@gmail.com>
2013-05-14 14:45:51 -0400

    Added region demo

---
+++ demos/region.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/ajhager/eng"
+)
+
+var (
+	batch   *eng.Batch
+	regions []*eng.Region
+)
+
+type Game struct {
+	*eng.Game
+}
+
+func (g *Game) Init(config *eng.Config) {
+	config.Title = "Region"
+}
+
+func (g *Game) Open() {
+	batch = eng.NewBatch()
+	texture := eng.NewTexture("data/bot.png")
+	regions = texture.Split(64, 64)
+}
+
+func (g *Game) Draw() {
+	batch.Begin()
+	batch.Draw(regions[0], 480, 300, 32, 32, 1, 1, 0, nil)
+	batch.End()
+}
+
+func main() {
+	eng.Run(new(Game))
+}
