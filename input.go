94d75f0356f992a0601e822b1f3abe404a95da19
Joseph Hager <ajhager@gmail.com>
2013-05-15 09:19:14 -0400

    Input comments

--- input.go
+++ input.go
@@ -1,9 +1,14 @@
+// Copyright 2013 Joseph Hager. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package eng
 
 import (
 	"github.com/go-gl/glfw"
 )
 
+// Keyboard key constants.
 type Key int
 
 var (
@@ -79,6 +84,7 @@ var (
 	Menu       = Key(glfw.KeyMenu)
 )
 
+// Mouse button constants.
 type Button int
 
 var (
