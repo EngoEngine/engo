2e6a21323f41567e8e6809aa022d75ca4d4559e6
Joseph Hager <ajhager@gmail.com>
2013-05-28 11:56:39 -0400

    Added mipmap texture support

--- eng.go
+++ eng.go
@@ -13,21 +13,26 @@ import (
 
 // Common OpenGL constants
 const (
-	BlendZero             = gl.ZERO
-	BlendOne              = gl.ONE
-	BlendSrcColor         = gl.SRC_COLOR
-	BlendOneMinusSrcColor = gl.ONE_MINUS_SRC_COLOR
-	BlendDstColor         = gl.DST_COLOR
-	BlendOneMinusDstColor = gl.ONE_MINUS_DST_COLOR
-	BlendSrcAlpha         = gl.SRC_ALPHA
-	BlendOneMinusSrcAlpha = gl.ONE_MINUS_SRC_ALPHA
-	BlendDstAlpha         = gl.DST_ALPHA
-	BlendOneMinusDstAlpha = gl.ONE_MINUS_DST_ALPHA
-	FilterNearest         = gl.NEAREST
-	FilterLinear          = gl.LINEAR
-	WrapClampToEdge       = gl.CLAMP_TO_EDGE
-	WrapRepeat            = gl.REPEAT
-	WrapMirroredRepeat    = gl.MIRRORED_REPEAT
+	BlendZero                  = gl.ZERO
+	BlendOne                   = gl.ONE
+	BlendSrcColor              = gl.SRC_COLOR
+	BlendOneMinusSrcColor      = gl.ONE_MINUS_SRC_COLOR
+	BlendDstColor              = gl.DST_COLOR
+	BlendOneMinusDstColor      = gl.ONE_MINUS_DST_COLOR
+	BlendSrcAlpha              = gl.SRC_ALPHA
+	BlendOneMinusSrcAlpha      = gl.ONE_MINUS_SRC_ALPHA
+	BlendDstAlpha              = gl.DST_ALPHA
+	BlendOneMinusDstAlpha      = gl.ONE_MINUS_DST_ALPHA
+	FilterNearest              = gl.NEAREST
+	FilterLinear               = gl.LINEAR
+	FilterMipMap               = gl.LINEAR_MIPMAP_LINEAR
+	FilterLinearMipMapLinear   = gl.LINEAR_MIPMAP_LINEAR
+	FilterNearestMipMapLinear  = gl.NEAREST_MIPMAP_LINEAR
+	FilterLinearMipMapNearest  = gl.LINEAR_MIPMAP_NEAREST
+	FilterNearestMipMapNearest = gl.NEAREST_MIPMAP_NEAREST
+	WrapClampToEdge            = gl.CLAMP_TO_EDGE
+	WrapRepeat                 = gl.REPEAT
+	WrapMirroredRepeat         = gl.MIRRORED_REPEAT
 )
 
 var (
--- texture.go
+++ texture.go
@@ -71,10 +71,11 @@ func NewTexture(data interface{}) *Texture {
 	gl.Enable(gl.TEXTURE_2D)
 	gl.BindTexture(gl.TEXTURE_2D, id)
 
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, WrapClampToEdge)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, WrapClampToEdge)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, FilterLinearMipMapNearest)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, FilterNearest)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.GENERATE_MIPMAP, gl.TRUE)
 
 	gl.PixelStorei(gl.UNPACK_ALIGNMENT, 1)
 	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.Sizei(width), gl.Sizei(height), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Pointer(&newm.Pix[0]))
