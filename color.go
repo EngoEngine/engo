8aad640f2bbef70d38a0af070a6e98de6c6209bf
Joseph Hager <ajhager@gmail.com>
2013-11-27 14:39:33 -0500

    Added the rest of the color blenders

--- color.go
+++ color.go
@@ -53,9 +53,29 @@ func (c *Color) Copy() *Color {
 	return &Color{c.R, c.G, c.B, c.A}
 }
 
+// Add = old + new
+func (c *Color) Add(o *Color) *Color {
+	return add(o, c)
+}
+
+// AddAlpha = old + alpha*new
+func (c *Color) AddAlpha(o *Color, a float32) *Color {
+	return addAlpha(o, c, a)
+}
+
+// Alpha = (1-alpha)*old + alpha*(new-old)
+func (c *Color) Alpha(o *Color, a float32) *Color {
+	return alpha(o, c, a)
+}
+
 // Blend satisfies the Blender interface by returning a constant copy.
 func (c *Color) Blend(o *Color, i, t int) *Color {
 	return c.Copy()
+}
+
+// Burn = old + new - white
+func (c *Color) Burn(o *Color) *Color {
+	return burn(o, c)
 }
 
 // Dodge = new / (white - old)
@@ -78,6 +98,26 @@ func (c *Color) Screen(o *Color) *Color {
 	return screen(o, c)
 }
 
+// Darken = MIN(old, new)
+func (c *Color) Darken(o *Color) *Color {
+	return darken(o, c)
+}
+
+// Lighten = MIN(old, new)
+func (c *Color) Lighten(o *Color) *Color {
+	return lighten(o, c)
+}
+
+// Scale = old * s
+func (c *Color) Scale(s float32) *Color {
+	return scale(c, s)
+}
+
+// RandScale scales the color a random amount.
+func (c *Color) RandScale() *Color {
+	return scale(c, rand.Float32())
+}
+
 // BlendFunc is a function that takes a color and returns a new color.
 type BlendFunc func(*Color) *Color
 
@@ -87,10 +127,38 @@ func (bf BlendFunc) Blend(o *Color, i, t int) *Color {
 	return bf(o)
 }
 
-// BlendMultiply
-func BlendMultiply(top *Color) BlendFunc {
+// BlendAdd
+func BlendAdd(top *Color) BlendFunc {
 	return func(bot *Color) *Color {
-		return multiply(top, bot)
+		return add(top, bot)
+	}
+}
+
+// BlendAddAlpha
+func BlendAddAlpha(top *Color, a float32) BlendFunc {
+	return func(bot *Color) *Color {
+		return addAlpha(top, bot, a)
+	}
+}
+
+// BlendAlpha
+func BlendAlpha(top *Color, a float32) BlendFunc {
+	return func(bot *Color) *Color {
+		return addAlpha(top, bot, a)
+	}
+}
+
+// BlendBurn
+func BlendBurn(top *Color) BlendFunc {
+	return func(bot *Color) *Color {
+		return burn(top, bot)
+	}
+}
+
+// BlendDarken
+func BlendDarken(top *Color) BlendFunc {
+	return func(bot *Color) *Color {
+		return darken(top, bot)
 	}
 }
 
@@ -98,6 +166,20 @@ func BlendMultiply(top *Color) BlendFunc {
 func BlendDodge(top *Color) BlendFunc {
 	return func(bot *Color) *Color {
 		return dodge(top, bot)
+	}
+}
+
+// BlendLighten
+func BlendLighten(top *Color) BlendFunc {
+	return func(bot *Color) *Color {
+		return lighten(top, bot)
+	}
+}
+
+// BlendMultiply
+func BlendMultiply(top *Color) BlendFunc {
+	return func(bot *Color) *Color {
+		return multiply(top, bot)
 	}
 }
 
@@ -112,6 +194,20 @@ func BlendOverlay(top *Color) BlendFunc {
 func BlendScreen(top *Color) BlendFunc {
 	return func(bot *Color) *Color {
 		return screen(top, bot)
+	}
+}
+
+// BlendRandScale
+func BlendRandScale() BlendFunc {
+	return func(bot *Color) *Color {
+		return bot.RandScale()
+	}
+}
+
+// BlendScale
+func BlendScale(s float32) BlendFunc {
+	return func(bot *Color) *Color {
+		return scale(bot, s)
 	}
 }
 
@@ -171,13 +267,58 @@ func overlayF(top, bot float32) float32 {
 }
 
 // Color blending functions
+func add(top, bot *Color) *Color {
+	return NewColor(
+		clampF(0, 1, bot.R+top.R),
+		clampF(0, 1, bot.G+top.G),
+		clampF(0, 1, bot.B+top.B),
+		clampF(0, 1, bot.A+top.A),
+	)
+}
+
+func addAlpha(top, bot *Color, a float32) *Color {
+	return NewColor(
+		clampF(0, 1, bot.R*a+top.R),
+		clampF(0, 1, bot.G*a+top.G),
+		clampF(0, 1, bot.B*a+top.B),
+		clampF(0, 1, bot.A*a+top.A),
+	)
+}
+
 func alpha(top, bot *Color, a float32) *Color {
 	a = clampF(0, 1, a)
 	return NewColor(bot.R+(top.R-bot.R)*a, bot.G+(top.G-bot.G)*a, bot.B+(top.B-bot.B)*a, bot.A+(top.A-bot.A)*a)
 }
 
+func burn(top, bot *Color) *Color {
+	return NewColor(
+		clampF(0, 1, bot.R+top.R-1),
+		clampF(0, 1, bot.G+top.G-1),
+		clampF(0, 1, bot.B+top.B-1),
+		clampF(0, 1, bot.A+top.A-1),
+	)
+}
+
+func darken(top, bot *Color) *Color {
+	return NewColor(
+		float32(math.Min(float64(top.R), float64(bot.R))),
+		float32(math.Min(float64(top.G), float64(bot.G))),
+		float32(math.Min(float64(top.B), float64(bot.B))),
+		float32(math.Min(float64(top.A), float64(bot.A))),
+	)
+}
+
 func dodge(top, bot *Color) *Color {
 	return NewColor(dodgeF(top.R, bot.R), dodgeF(top.G, bot.G), dodgeF(top.B, bot.B), dodgeF(top.A, bot.A))
+}
+
+func lighten(top, bot *Color) *Color {
+	return NewColor(
+		float32(math.Max(float64(top.R), float64(bot.R))),
+		float32(math.Max(float64(top.G), float64(bot.G))),
+		float32(math.Max(float64(top.B), float64(bot.B))),
+		float32(math.Max(float64(top.A), float64(bot.A))),
+	)
 }
 
 func multiply(top, bot *Color) *Color {
@@ -186,6 +327,10 @@ func multiply(top, bot *Color) *Color {
 
 func overlay(top, bot *Color) *Color {
 	return NewColor(overlayF(top.R, bot.R), overlayF(top.G, bot.G), overlayF(top.B, bot.B), overlayF(top.A, bot.A))
+}
+
+func scale(bot *Color, s float32) *Color {
+	return NewColor(bot.R*s, bot.G*s, bot.B*s, bot.A*s)
 }
 
 func screen(top, bot *Color) *Color {
