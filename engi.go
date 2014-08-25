607aab45b678e1a6d567b264326be8f162ce4b9e
Joseph Hager <ajhager@gmail.com>
2014-08-24 23:58:19 -0400

    Move out responder into its own file

--- engi.go
+++ engi.go
@@ -13,34 +13,6 @@ var (
 	GL        *gl2
 )
 
-type Responder interface {
-	Render()
-	Resize(width, height int)
-	Preload()
-	Setup()
-	Update(dt float32)
-	Mouse(x, y float32, action Action)
-	Scroll(amount float32)
-	Key(key Key, modifier Modifier, action Action)
-	Type(char rune)
-}
-
-type Game struct{}
-
-func (g *Game) Preload()                          {}
-func (g *Game) Setup()                            {}
-func (g *Game) Update(dt float32)                 {}
-func (g *Game) Render()                           {}
-func (g *Game) Resize(w, h int)                   {}
-func (g *Game) Mouse(x, y float32, action Action) {}
-func (g *Game) Scroll(amount float32)             {}
-func (g *Game) Key(key Key, modifier Modifier, action Action) {
-	if key == Escape {
-		Exit()
-	}
-}
-func (g *Game) Type(char rune) {}
-
 func Open(title string, width, height int, fullscreen bool, r Responder) {
 	responder = r
 	Time = NewClock()
