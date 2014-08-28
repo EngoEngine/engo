a1b8daac875485c08e0254c83a0112382edd8796
Joseph Hager <ajhager@gmail.com>
2014-08-28 00:41:24 -0400

    Add back a simple font support

--- demos/hello.go
+++ demos/hello.go
@@ -8,21 +8,25 @@ type Game struct {
 	*engi.Game
 	bot   engi.Drawable
 	batch *engi.Batch
+	font  *engi.Font
 }
 
 func (game *Game) Preload() {
 	engi.Files.Add("bot", "data/icon.png")
-	game.batch = engi.NewBatch(1024, 640)
+	engi.Files.Add("font", "data/font.png")
+	game.batch = engi.NewBatch(engi.Width(), engi.Height())
 }
 
 func (game *Game) Setup() {
 	engi.SetBg(0x2d3739)
 	game.bot = engi.Files.Image("bot")
+	game.font = engi.NewGridFont(engi.Files.Image("font"), 20, 20)
 }
 
 func (game *Game) Render() {
 	game.batch.Begin()
-	game.batch.Draw(game.bot, 512, 320, 0.5, 0.5, 20, 20, 0, 0xffffff, 1)
+	game.font.Print(game.batch, "ENGI", 475, 200, 0xffffff)
+	game.batch.Draw(game.bot, 512, 320, 0.5, 0.5, 10, 10, 0, 0xffffff, 1)
 	game.batch.End()
 }
 
