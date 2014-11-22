ab7d97e01d4cc9fa1aae497d328229eb56b8bdec
harrison <harrison@theshoebridges.com>
2014-11-22 20:57:02 +1100

    Got the pong clone to a playable point!

--- demos/pong/pong.go
+++ demos/pong/pong.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/paked/engi"
 	"log"
+	"math/rand"
 )
 
 type PongGame struct {
@@ -12,6 +14,7 @@ type PongGame struct {
 func (pong PongGame) Preload() {
 	engi.Files.Add("ball", "assets/ball.png")
 	engi.Files.Add("paddle", "assets/paddle.png")
+	engi.Files.Add("font", "assets/font.png")
 }
 
 func (pong *PongGame) Setup() {
@@ -21,6 +24,7 @@ func (pong *PongGame) Setup() {
 	pong.AddSystem(&SpeedSystem{})
 	pong.AddSystem(&ControlSystem{})
 	pong.AddSystem(&BallSystem{})
+	pong.AddSystem(&ScoreSystem{})
 
 	ball := engi.NewEntity([]string{"RenderSystem", "CollisionSystem", "SpeedSystem", "BallSystem"})
 	ballTexture := engi.Files.Image("ball")
@@ -35,6 +39,14 @@ func (pong *PongGame) Setup() {
 	ball.AddComponent(&ballSpeed)
 	pong.AddEntity(ball)
 
+	score := engi.NewEntity([]string{"RenderSystem", "ScoreSystem"})
+	scoreTex := engi.NewGridFont(engi.Files.Image("font"), 20, 20)
+	scoreRender := engi.NewRenderComponent(scoreTex, engi.Point{1, 1}, "YOLO <3")
+	scoreSpace := engi.SpaceComponent{engi.Point{100, 100}, 10, 10}
+	score.AddComponent(&scoreRender)
+	score.AddComponent(&scoreSpace)
+	pong.AddEntity(score)
+
 	schemes := []string{"WASD", ""}
 	for i := 0; i < 2; i++ {
 		paddle := engi.NewEntity([]string{"RenderSystem", "CollisionSystem", "ControlSystem"})
@@ -42,7 +54,6 @@ func (pong *PongGame) Setup() {
 		paddleRender := engi.NewRenderComponent(paddleTexture, engi.Point{2, 2}, "paddle")
 		x := float32(0)
 		if i != 0 {
-			// x = (engi.Width() / 2) - (paddleTexture.Width() * paddleRender.Scale.X)
 			x = 800 - 16
 		}
 		paddleSpace := engi.SpaceComponent{engi.Point{x, (engi.Height() - paddleTexture.Height()) / 2}, paddleRender.Scale.X * paddleTexture.Width(), paddleRender.Scale.Y * paddleTexture.Height()}
@@ -112,8 +123,12 @@ func (bs *BallSystem) Update(entity *engi.Entity, dt float32) {
 	speed, hasSpeed := entity.GetComponent("SpeedComponent").(*SpeedComponent)
 	if hasSpace && hasSpeed {
 		if space.Position.X < 0 {
-			space.Position.X = 0
-			speed.X *= -1
+			engi.TheWorld.Mailbox.Dispatch(ScoreMessage{1})
+
+			space.Position.X = 400 - 16
+			space.Position.Y = 400 - 16
+			speed.X = 300 * rand.Float32()
+			speed.Y = 300 * rand.Float32()
 		}
 
 		if space.Position.Y < 0 {
@@ -122,16 +137,23 @@ func (bs *BallSystem) Update(entity *engi.Entity, dt float32) {
 		}
 
 		if space.Position.X > 800 {
-			space.Position.X = 800 - 16
-			speed.X *= -1
+			engi.TheWorld.Mailbox.Dispatch(ScoreMessage{2})
+
+			space.Position.X = 400 - 16
+			space.Position.Y = 400 - 16
+			speed.X = 300 * rand.Float32()
+			speed.Y = 300 * rand.Float32()
 		}
 
 		if space.Position.Y > 800 {
 			space.Position.Y = 800 - 16
-			// speed.Y *= -1
+
+			speed.Y *= -1
 		}
 	}
 }
+
+// func ResetBall(space, speed)
 
 type ControlSystem struct {
 	*engi.System
@@ -161,11 +183,11 @@ func (c *ControlSystem) Update(entity *engi.Entity, dt float32) {
 		}
 
 		if up {
-			space.Position.Y -= 600 * dt
+			space.Position.Y -= 800 * dt
 		}
 
 		if down {
-			space.Position.Y += 600 * dt
+			space.Position.Y += 800 * dt
 		}
 	}
 }
@@ -176,6 +198,57 @@ type ControlComponent struct {
 
 func (cs ControlComponent) Name() string {
 	return "ControlComponent"
+}
+
+type ScoreSystem struct {
+	*engi.System
+	PlayerOneScore, PlayerTwoScore int
+}
+
+func (score *ScoreSystem) Name() string {
+	return "ScoreSystem"
+}
+
+func (c *ScoreSystem) New() {
+	c.System = &engi.System{}
+	engi.TheWorld.Mailbox.Listen("ScoreMessage", c)
+}
+
+func (sc *ScoreSystem) Receive(message engi.Message) {
+	scoreMessage, isScore := message.(ScoreMessage)
+	if !isScore {
+		return
+	}
+
+	if scoreMessage.Player != 1 {
+		sc.PlayerOneScore += 1
+	} else {
+		sc.PlayerTwoScore += 1
+	}
+}
+
+func (c *ScoreSystem) Update(entity *engi.Entity, dt float32) {
+
+	render, hasRender := entity.GetComponent("RenderComponent").(*engi.RenderComponent)
+	space, hasSpace := entity.GetComponent("SpaceComponent").(*engi.SpaceComponent)
+	if !hasRender || !hasSpace {
+		return
+	}
+
+	render.Label = fmt.Sprintf("%v vs %v", c.PlayerOneScore, c.PlayerTwoScore)
+
+	width := len(render.Label) * 20
+
+	space.Position.X = float32(400 - (width / 2))
+
+}
+
+type ScoreMessage struct {
+	Player int
+}
+
+func (score ScoreMessage) Type() string {
+	return "ScoreMessage"
 }
 
 func main() {
