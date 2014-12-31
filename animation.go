21295b2199810eb5b6d1771269c11768fca2efb4
Harrison <harrison@theshoebridges.com>
2014-12-31 19:49:22 +1100

    Added documentation to the animation related stuffs

--- animation.go
+++ animation.go
@@ -1,16 +1,28 @@
+// Copyright 2014 Harrison Shoebridge. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package engi
 
 import (
 	"log"
 )
 
+// Spritesheet is a class that stores a set of tiles from a file, used by tilemaps and animations
 type Spritesheet struct {
-	texture               *Texture
-	CellWidth, CellHeight int
-	cache                 map[int]*Region
+	texture               *Texture        // The original texture
+	CellWidth, CellHeight int             // The dimensions of the cells
+	cache                 map[int]*Region // The cell cache cells
 }
 
-func (s Spritesheet) Cell(i int) *Region {
+// Simple handler for creating a new spritesheet from a file
+// textureName is the name of a texture already preloaded with engi.Files.Add
+func NewSpritesheet(textureName string, cellWidth, cellHeight int) *Spritesheet {
+	return &Spritesheet{texture: Files.Image(textureName), CellWidth: cellWidth, CellHeight: cellHeight, cache: make(map[int]*Region)}
+}
+
+// Get the region at the index i, updates and pulls from cache if need be
+func (s *Spritesheet) Cell(i int) *Region {
 	if r := s.cache[i]; r != nil {
 		return r
 	}
@@ -18,30 +30,30 @@ func (s Spritesheet) Cell(i int) *Region {
 	return s.cache[i]
 }
 
+// The amount of tiles on the x-axis of the spritesheet
 func (s Spritesheet) Width() float32 {
 	return s.texture.Width() / float32(s.CellWidth)
 }
 
+// The amount of tiles on the y-axis of the spritesheet
 func (s Spritesheet) Height() float32 {
 	return s.texture.Height() / float32(s.CellHeight)
 }
 
-func NewSpritesheet(filename string, cellsize int) *Spritesheet {
-	return &Spritesheet{texture: Files.Image(filename), CellWidth: cellsize, CellHeight: cellsize, cache: make(map[int]*Region)}
+// Component that controls animation in rendering entities
+type AnimationComponent struct {
+	Index            int              // What frame in the is being used
+	_index           int              // The index of the tile that should currently be being displayed
+	Rate             float32          // How often frames should increment, in seconds.
+	change           float32          // The time since the last incrementation
+	S                *Spritesheet     // Pointer to the source spritesheet
+	Animations       map[string][]int // All possible animations
+	CurrentAnimation []int            // The currently animation
 }
 
+// Create a new pointer to AnimationComponent
 func NewAnimationComponent() *AnimationComponent {
 	return &AnimationComponent{Animations: make(map[string][]int)}
-}
-
-type AnimationComponent struct {
-	Index            int
-	_index           int
-	Rate             float32
-	Change           float32
-	S                *Spritesheet
-	Animations       map[string][]int
-	CurrentAnimation []int
 }
 
 func (ac *AnimationComponent) SelectAnimation(name string) {
@@ -52,21 +64,23 @@ func (ac *AnimationComponent) AddAnimation(name string, indexes []int) {
 	ac.Animations[name] = indexes
 }
 
+// Increment the frame
 func (ac *AnimationComponent) Increment() {
 	if len(ac.CurrentAnimation) == 0 {
 		log.Println("No data for this animation")
 		return
 	}
 
-	// log.Println("Incrementing")
 	ac.Index += 1
 	if ac.Index >= len(ac.CurrentAnimation) {
 		ac.Index = 0
 	}
 	ac._index = ac.CurrentAnimation[ac.Index]
-	ac.Change = 0
+	ac.change = 0
+
 }
 
+//Bug(me) Don't need to use _index at all, use ac.CurrentAnimation[ac.Index] instead. Set ac.Index as a private member variable
 func (ac *AnimationComponent) Cell() *Region {
 	return ac.S.Cell(ac._index)
 }
@@ -97,8 +111,8 @@ func (a *AnimationSystem) Update(e *Entity, dt float32) {
 		return
 	}
 
-	ac.Change += dt
-	if ac.Change >= ac.Rate {
+	ac.change += dt
+	if ac.change >= ac.Rate {
 		ac.Increment()
 		r.Display = ac.Cell()
 	}
--- demos/hello.go
+++ demos/hello.go
@@ -33,7 +33,7 @@ func (game *GameWorld) Setup() {
 
 	entity := engi.NewEntity([]string{"RenderSystem", "MovingSystem", "CollisionSystem", "AnimationSystem"})
 	texture := engi.Files.Image("bot")
-	spritesheet := engi.NewSpritesheet("sample", 16)
+	spritesheet := engi.NewSpritesheet("sample", 16, 16)
 
 	animation := engi.NewAnimationComponent()
 	animation.Rate = .1
@@ -59,7 +59,7 @@ func (game *GameWorld) Setup() {
 	game.AddEntity(text)
 
 	gameMap := engi.NewEntity([]string{"RenderSystem", "CollisionSystem"})
-	tilemap := engi.NewTilemap([][]string{{"1", "2", "1"}, {"0", "0", "2"}, {"0", "0", "1"}, {"2", "1", "2"}}, engi.Files.Image("sheet"), 16)
+	tilemap := engi.NewTilemap([][]string{{"1", "2", "3"}, {"4", "5", "1"}, {"2", "3", "4"}, {"5", "1", "2"}}, engi.Files.Image("sheet"), 16)
 	mapRender := engi.NewRenderComponent(tilemap, engi.Point{1, 1}, "map")
 	mapSpace := engi.SpaceComponent{engi.Point{100, 100}, 0, 0}
 	gameMap.AddComponent(&mapRender)
