a0fecc7bfab9f8d66ef9a04af6db1172feb61764
Harrison <harrison@theshoebridges.com>
2015-04-29 22:21:05 +1000

    hello: add system example

--- demos/hello.go
+++ demos/hello.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"github.com/paked/engi"
 	"log"
+	"math/rand"
+
+	"github.com/paked/engi"
 )
 
 var World *GameWorld
@@ -26,8 +28,9 @@ func (game *GameWorld) Setup() {
 	engi.SetBg(0x2d3739)
 
 	game.AddSystem(&engi.RenderSystem{})
+	game.AddSystem(&ScaleSystem{})
 
-	guy := engi.NewEntity([]string{"RenderSystem", "ControlSystem", "CollisionSystem", "DeathSystem"})
+	guy := engi.NewEntity([]string{"RenderSystem", "ScaleSystem"})
 	texture := engi.Files.Image("bot")
 	render := engi.NewRenderComponent(texture, engi.Point{8, 8}, "guy")
 	collision := engi.CollisionComponent{Solid: true, Main: true}
@@ -42,7 +45,38 @@ func (game *GameWorld) Setup() {
 	guy.AddComponent(&collision)
 
 	game.AddEntity(guy)
+}
 
+type ScaleSystem struct {
+	*engi.System
+}
+
+func (s *ScaleSystem) Name() string {
+	return "ScaleSystem"
+}
+
+func (s *ScaleSystem) New() {
+	s.System = &engi.System{}
+}
+
+func (c *ScaleSystem) Update(e *engi.Entity, dt float32) {
+	var render *engi.RenderComponent
+	if !e.GetComponent(&render) {
+		return
+	}
+	var mod float32
+
+	if rand.Int()%2 == 0 {
+		mod = 0.1
+	} else {
+		mod = -0.1
+	}
+
+	if render.Scale.X+mod >= 15 || render.Scale.X+mod <= 1 {
+		mod *= -1
+	}
+
+	render.Scale.AddScalar(mod)
 }
 
 func main() {
