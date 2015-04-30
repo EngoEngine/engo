f236ac084b927bb87125fe28f3e6fe5c4dd566a3
Harrison <harrison@theshoebridges.com>
2015-04-30 11:55:15 +1000

    +tilemap: add basic tilemap example

---
+++ demos/tilemap.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+
+	"github.com/paked/engi"
+)
+
+var World *GameWorld
+
+type GameWorld struct {
+	engi.World
+}
+
+func (game *GameWorld) Preload() {
+	engi.Files.Add(
+		engi.NewResource("sheet", "data/sheet.png"),
+	)
+
+	log.Println("Preloaded")
+}
+
+func (game *GameWorld) Setup() {
+	engi.SetBg(0x2d3739)
+
+	game.AddSystem(&engi.RenderSystem{})
+
+	gameMap := engi.NewEntity([]string{"RenderSystem"})
+	tilemap := engi.NewTilemap(
+		[][]string{
+			{"0", "2", "0"},
+			{"4", "5", "1"},
+			{"2", "3", "4"},
+			{"5", "1", "2"}},
+		engi.Files.Image("sheet"), 16)
+
+	mapRender := engi.NewRenderComponent(tilemap, engi.Point{1, 1}, "map")
+	mapSpace := engi.SpaceComponent{engi.Point{100, 100}, 0, 0}
+	gameMap.AddComponent(&mapRender)
+	gameMap.AddComponent(&mapSpace)
+
+	game.AddEntity(gameMap)
+}
+
+func main() {
+	World = &GameWorld{}
+	engi.Open("Hello", 1024, 640, false, World)
+}
