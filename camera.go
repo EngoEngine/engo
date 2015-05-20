497039f2d02c31778ec8490d0c7bc9d78dd0525a
TheBerryBeast <theberrybeast@gmail.com>
2015-05-20 17:51:29 -0400

    system.go:renamed cam var, camera.go: camera within world bounds

--- camera.go
+++ camera.go
@@ -6,7 +6,7 @@ package engi
 
 // A rather basic camera
 type Camera struct {
-	pos, to Point
+	pos, to  Point
 	tracking *Entity // The entity that is currently being followed
 }
 
@@ -16,7 +16,7 @@ func (cam *Camera) FollowEntity(entity *Entity) {
 	if !cam.tracking.GetComponent(&space) {
 		return
 	}
-	
+
 	cam.to = space.Position
 	cam.centerCam(Width(), Height(), 1, space)
 }
@@ -32,12 +32,16 @@ func (cam *Camera) Update(dt float32) {
 	}
 }
 
-func (cam *Camera) centerCam( width, height, lerp float32, space *SpaceComponent) {
+func (cam *Camera) centerCam(width, height, lerp float32, space *SpaceComponent) {
+	lvl := Files.Level("test.tmx")
+
+	maxX := float32(lvl.Width * lvl.TileWidth)
+	//maxY := float32(lvl.Height * lvl.TileHeight)
 
 	cam.to.X += ((space.Position.X + space.Width/2) - (cam.to.X + width/2)) * lerp
 	cam.to.Y += ((space.Position.Y + space.Height) - (cam.to.Y + height)) * lerp
-	
-	if(!(cam.to.X + width >= 1600) && !(cam.to.X <= 0 )) {
+
+	if !(cam.to.X+width >= maxX) && !(cam.to.X <= 0) {
 		cam.pos = cam.to
 	}
 }
