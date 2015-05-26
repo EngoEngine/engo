05828faea93fc4c1d510cc8d9e8ea86642528225
Harrison <harrison@theshoebridges.com>
2015-05-27 07:47:49 +1000

    camera: fix camera logic

--- camera.go
+++ camera.go
@@ -48,17 +48,22 @@ func (cam *Camera) centerCam(width, height, lerp float32, space *SpaceComponent)
 	if dHeight == 0 {
 		dHeight = 200
 	}
+
 	min, max := Point{}, Point{}
 
 	min.X, min.Y = cam.to.X-(dWidth/2), cam.to.Y-(dHeight/2)
 	max.X, max.Y = min.X+dWidth, min.Y+dHeight
 
-	if cam.pos.X < min.X || cam.pos.X > min.Y {
+	if cam.pos.X < min.X {
 		cam.pos.X = Clamp(floorFloat32(min.X), WorldBounds.Min.X, WorldBounds.Max.X-width)
+	} else if cam.pos.X > max.X {
+		cam.pos.X = Clamp(floorFloat32(max.X), WorldBounds.Min.X, WorldBounds.Max.X-width)
 	}
 
-	if cam.pos.Y < max.X || cam.pos.Y > max.Y {
+	if cam.pos.Y < min.X {
 		cam.pos.Y = Clamp(floorFloat32(min.Y), WorldBounds.Min.Y, WorldBounds.Max.Y-height)
+	} else if cam.pos.Y > max.Y {
+		cam.pos.Y = Clamp(floorFloat32(max.Y), WorldBounds.Min.Y, WorldBounds.Max.Y-height)
 	}
 }
 
