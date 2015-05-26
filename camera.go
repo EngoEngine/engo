1780f488da0eccb4b938149a53e539d446c9d20f
Harrison <harrison@theshoebridges.com>
2015-05-27 07:19:46 +1000

    camera: add changeable deadzone size

--- camera.go
+++ camera.go
@@ -8,8 +8,9 @@ import "math"
 
 // A rather basic camera
 type Camera struct {
-	pos, to  Point
-	tracking *Entity // The entity that is currently being followed
+	DeadzoneSize Point
+	pos, to      Point
+	tracking     *Entity // The entity that is currently being followed
 }
 
 func (cam *Camera) FollowEntity(entity *Entity) {
@@ -24,7 +25,6 @@ func (cam *Camera) FollowEntity(entity *Entity) {
 }
 
 func (cam *Camera) Update(dt float32) {
-	// maxPoints := 8
 	if cam.tracking != nil {
 		var space *SpaceComponent
 		if !cam.tracking.GetComponent(&space) {
@@ -38,8 +38,19 @@ func (cam *Camera) centerCam(width, height, lerp float32, space *SpaceComponent)
 	cam.to.X += ((space.Position.X + space.Width/2) - (cam.to.X + width/2)) * lerp
 	cam.to.Y += ((space.Position.Y + space.Height/2) - (cam.to.Y + height/2)) * lerp
 
-	minX, minY := cam.to.X-100, cam.to.Y-100
-	maxX, maxY := minX+200, minY+200
+	dWidth := cam.DeadzoneSize.X
+	dHeight := cam.DeadzoneSize.Y
+
+	if dWidth == 0 {
+		dWidth = 200
+	}
+
+	if dHeight == 0 {
+		dHeight = 200
+	}
+
+	minX, minY := cam.to.X-(dWidth/2), cam.to.Y-(dHeight/2)
+	maxX, maxY := minX+dWidth, minY+dHeight
 
 	if cam.pos.X < minX || cam.pos.X > maxX {
 		cam.pos.X = Clamp(floorFloat32(minX), WorldBounds.Min.X, WorldBounds.Max.X-width)
