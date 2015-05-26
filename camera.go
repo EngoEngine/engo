1c705bdc63d2b4ddc2945927e4accdab41232eaf
Harrison <harrison@theshoebridges.com>
2015-05-27 07:05:09 +1000

    camera: remove redundant variables, shorten ifs

--- camera.go
+++ camera.go
@@ -38,28 +38,16 @@ func (cam *Camera) centerCam(width, height, lerp float32, space *SpaceComponent)
 	cam.to.X += ((space.Position.X + space.Width/2) - (cam.to.X + width/2)) * lerp
 	cam.to.Y += ((space.Position.Y + space.Height/2) - (cam.to.Y + height/2)) * lerp
 
-	posX := cam.to.X
-	posY := cam.to.Y
-
-	minX, minY := posX-100, posY-100
+	minX, minY := cam.to.X-100, cam.to.Y-100
 	maxX, maxY := minX+200, minY+200
 
-	if cam.pos.X < minX {
-		cam.pos.X = Clamp(floorFloat32(minX), 0, WorldBounds.Max.X-width)
+	if cam.pos.X < minX || cam.pos.X > maxX {
+		cam.pos.X = Clamp(floorFloat32(minX), WorldBounds.Min.X, WorldBounds.Max.X-width)
 	}
 
-	if cam.pos.X > maxX {
-		cam.pos.X = Clamp(floorFloat32(maxX), 0, WorldBounds.Max.X-width)
+	if cam.pos.Y < minY || cam.pos.Y > maxY {
+		cam.pos.Y = Clamp(floorFloat32(minY), WorldBounds.Min.Y, WorldBounds.Max.Y-height)
 	}
-
-	if posY < minY {
-		cam.pos.Y = Clamp(floorFloat32(minY), 0, WorldBounds.Max.Y-height)
-	}
-
-	if posY > maxY {
-		cam.pos.Y = Clamp(floorFloat32(maxY), 0, WorldBounds.Max.Y-height)
-	}
-
 }
 
 func floorFloat32(i float32) float32 {
