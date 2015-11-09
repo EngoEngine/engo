ce760ff002de5e65b19832e23b9064de8c48825a
Mateusz Szczap <mati@sz.home.pl>
2015-11-09 23:10:40 +0100

    Improve Animation Demo.

--- demos/animation/anim.go
+++ demos/animation/anim.go
@@ -5,27 +5,26 @@ import (
 )
 
 var (
-	zoomSpeed float32 = -0.125
-	World     *GameWorld
+	zoomSpeed   float32 = -0.125
+	World       *GameWorld
+	RunAction   *engi.AnimationAction
+	WalkAction  *engi.AnimationAction
+	StopAction  *engi.AnimationAction
+	SkillAction *engi.AnimationAction
+	DieAction   *engi.AnimationAction
+	actions     []*engi.AnimationAction
 )
 
-type GameWorld struct {
-	RUN_ACTION   *engi.AnimationAction
-	WALK_ACTION  *engi.AnimationAction
-	STOP_ACTION  *engi.AnimationAction
-	SKILL_ACTION *engi.AnimationAction
-	DIE_ACTION   *engi.AnimationAction
-	actions      []*engi.AnimationAction
-}
+type GameWorld struct{}
 
 func (game *GameWorld) Preload() {
 	engi.Files.Add("assets/hero.png")
-	game.STOP_ACTION = &engi.AnimationAction{Name: "stop", Frames: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}}
-	game.RUN_ACTION = &engi.AnimationAction{Name: "run", Frames: []int{16, 17, 18, 19, 20, 21}}
-	game.WALK_ACTION = &engi.AnimationAction{Name: "move", Frames: []int{11, 12, 13, 14, 15}}
-	game.SKILL_ACTION = &engi.AnimationAction{Name: "skill", Frames: []int{44, 45, 46, 47, 48, 49, 50, 51, 52, 53}}
-	game.DIE_ACTION = &engi.AnimationAction{Name: "die", Frames: []int{28, 29, 30}}
-	game.actions = []*engi.AnimationAction{game.DIE_ACTION, game.STOP_ACTION, game.WALK_ACTION, game.RUN_ACTION, game.SKILL_ACTION}
+	StopAction = &engi.AnimationAction{Name: "stop", Frames: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}}
+	RunAction = &engi.AnimationAction{Name: "run", Frames: []int{16, 17, 18, 19, 20, 21}}
+	WalkAction = &engi.AnimationAction{Name: "move", Frames: []int{11, 12, 13, 14, 15}}
+	SkillAction = &engi.AnimationAction{Name: "skill", Frames: []int{44, 45, 46, 47, 48, 49, 50, 51, 52, 53}}
+	DieAction = &engi.AnimationAction{Name: "die", Frames: []int{28, 29, 30}}
+	actions = []*engi.AnimationAction{DieAction, StopAction, WalkAction, RunAction, SkillAction}
 }
 
 func (game *GameWorld) Setup(w *engi.World) {
@@ -33,30 +32,55 @@ func (game *GameWorld) Setup(w *engi.World) {
 
 	w.AddSystem(&engi.RenderSystem{})
 	w.AddSystem(&engi.AnimationSystem{})
+	w.AddSystem(&ControlSystem{})
 	w.AddSystem(engi.NewMouseZoomer(zoomSpeed))
 
 	spriteSheet := engi.NewSpritesheetFromFile("hero.png", 150, 150)
 
-	w.AddEntity(game.CreateEntity(&engi.Point{0, 0}, spriteSheet, game.RUN_ACTION))
-	w.AddEntity(game.CreateEntity(&engi.Point{300, 0}, spriteSheet, game.WALK_ACTION))
-	w.AddEntity(game.CreateEntity(&engi.Point{600, 0}, spriteSheet, game.STOP_ACTION))
-	w.AddEntity(game.CreateEntity(&engi.Point{900, 0}, spriteSheet, game.SKILL_ACTION))
-	w.AddEntity(game.CreateEntity(&engi.Point{1200, 0}, spriteSheet, game.DIE_ACTION))
+	w.AddEntity(game.CreateEntity(&engi.Point{0, 0}, spriteSheet, StopAction))
 }
 
 func (game *GameWorld) CreateEntity(point *engi.Point, spriteSheet *engi.Spritesheet, action *engi.AnimationAction) *engi.Entity {
-	entity := engi.NewEntity([]string{"AnimationSystem", "RenderSystem"})
+	entity := engi.NewEntity([]string{"AnimationSystem", "RenderSystem", "ControlSystem"})
 
-	space := &engi.SpaceComponent{*point, 0, 0}
+	space := &engi.SpaceComponent{*point, 150, 150}
 	render := engi.NewRenderComponent(spriteSheet.Cell(action.Frames[0]), engi.Point{3, 3}, "hero")
 	animation := engi.NewAnimationComponent(spriteSheet.Renderables(), 0.1)
-	animation.AddAnimationActions(game.actions)
+	animation.AddAnimationActions(actions)
 	animation.SelectAnimationByAction(action)
 	entity.AddComponent(render)
 	entity.AddComponent(space)
 	entity.AddComponent(animation)
 
 	return entity
+}
+
+type ControlSystem struct {
+	*engi.System
+}
+
+func (ControlSystem) Type() string {
+	return "ControlSystem"
+}
+
+func (c *ControlSystem) New() {
+	c.System = engi.NewSystem()
+}
+
+func (c *ControlSystem) Update(entity *engi.Entity, dt float32) {
+	var a *engi.AnimationComponent
+
+	if !entity.GetComponent(&a) {
+		return
+	}
+
+	if engi.Keys.Get(engi.ArrowRight).Down() {
+		a.SelectAnimationByAction(WalkAction)
+	} else if engi.Keys.Get(engi.Space).Down() {
+		a.SelectAnimationByAction(SkillAction)
+	} else {
+		a.SelectAnimationByAction(StopAction)
+	}
 }
 
 func main() {
