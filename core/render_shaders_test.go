5b9f02d40741582a043ad9399c70a9ef9637b2d1
Etienne Bruines <etienne@bruines.com>
2016-05-12 20:14:19 +0200

    Added additional tests for shader compilation

--- core/render_shaders_test.go
+++ core/render_shaders_test.go
@@ -8,7 +8,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestShaders(t *testing.T) {
+// TestShadersInitialization tests whether all registered `Shader`s will `Setup` without any errors
+func TestShadersInitialization(t *testing.T) {
 	engo.SetHeadless(true)
 	engo.CreateWindow("", 100, 100, false, 1)
 	w := &ecs.World{}
@@ -16,3 +17,56 @@ func TestShaders(t *testing.T) {
 	err := initShaders(w)
 	assert.NoError(t, err)
 }
+
+// TestShaderCompilation tests whether the LoadShader method will indeed report errors iff
+// (one of) the GLSL-shaders is incorrect.
+func TestShaderCompilation(t *testing.T) {
+	engo.SetHeadless(true)
+	engo.CreateWindow("", 100, 100, false, 1)
+	var err error
+
+	_, err = LoadShader(correctVertShader, correctFragShader)
+	assert.NoError(t, err)
+
+	_, err = LoadShader(correctVertShader, incorrectFragShader)
+	assert.IsType(t, FragmentShaderCompilationError{}, err)
+
+	_, err = LoadShader(incorrectVertShader, correctFragShader)
+	assert.IsType(t, VertexShaderCompilationError{}, err)
+
+	_, err = LoadShader(incorrectVertShader, incorrectFragShader)
+	assert.Error(t, err) // don't really care which one it is
+}
+
+var correctVertShader = `
+attribute vec2 in_Position;
+attribute vec4 in_Color;
+
+uniform mat3 matrixProjection;
+uniform mat3 matrixView;
+uniform mat3 matrixModel;
+
+void main() {
+
+  vec3 matr = matrixProjection * matrixView * matrixModel * vec3(in_Position, 1.0);
+  gl_Position = vec4(matr.xy, 0, matr.z);
+}
+`
+var correctFragShader = `
+#ifdef GL_ES
+#define LOWP lowp
+precision mediump float;
+#else
+#define LOWP
+#endif
+
+void main (void) {
+  gl_FragColor = vec4(1.0); // all-white
+}
+`
+var incorrectVertShader = `
+this is incorrect GLSL syntax
+`
+var incorrectFragShader = `
+this is incorrect GLSL syntax
+`
