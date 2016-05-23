b65841ebcb7ac02ca6751c687af1c59955cec9be
Etienne Bruines <etienne@bruines.com>
2016-05-23 21:53:01 +0200

    Font atlas support
    Fixes #232

--- common/font.go
+++ common/font.go
@@ -9,10 +9,13 @@ import (
 	"log"
 
 	"engo.io/engo"
+	"engo.io/gl"
+	"github.com/chai2010/webp"
 	"github.com/golang/freetype"
 	"github.com/golang/freetype/truetype"
 	"golang.org/x/image/font"
 	"golang.org/x/image/math/fixed"
+	"os"
 )
 
 var (
@@ -145,3 +148,152 @@ func (f *Font) Render(text string) Texture {
 	imObj := NewImageObject(nrgba)
 	return NewTextureSingle(imObj)
 }
+
+// generateFontAtlas generates the font atlas for this given font, using the first `c` Unicode characters.
+func (f *Font) generateFontAtlas(c int) FontAtlas {
+	atlas := FontAtlas{
+		XLocation: make([]float32, c),
+		YLocation: make([]float32, c),
+		Width:     make([]float32, c),
+		Height:    make([]float32, c),
+	}
+
+	var (
+		int26Width  fixed.Int26_6
+		int26Height fixed.Int26_6
+
+		totalHeight fixed.Int26_6
+
+		fupe        = fixed.Int26_6(f.TTF.FUnitsPerEm())
+		maxYBearing = fixed.Int26_6(0)
+
+		currentX float32
+		maxX     float32
+
+		totalString string // TODO; string is immutable, so this is relatively inefficient
+		subString   string
+
+		drawCurY int
+	)
+
+	// The "full image"
+	nrgba := image.NewNRGBA(image.Rect(0, 0, 1200, 5000)) // way too big; hopefully
+
+	var lastI int
+	for i := 0; i < c; i++ {
+		char := rune(i)
+		totalString += string(char)
+		subString += string(char)
+		atlas.XLocation[char] = currentX
+		atlas.YLocation[char] = float32(drawCurY)
+
+		idx := f.TTF.Index(char)
+		hm := f.TTF.HMetric(fupe, idx)
+		vm := f.TTF.VMetric(fupe, idx)
+
+		g := truetype.GlyphBuf{}
+		err := g.Load(f.TTF, fupe, idx, font.HintingNone)
+		if err != nil {
+			log.Println("Error creating font atlas:", err)
+			return atlas
+		}
+
+		int26Width += hm.AdvanceWidth
+
+		//atlas.Width[char] = float32(hm.AdvanceWidth * fixed.Int26_6(f.Size) / fupe)
+		atlas.Width[char] = float32(g.AdvanceWidth * fixed.Int26_6(f.Size) / fupe)
+
+		currentX = float32(int26Width * fixed.Int26_6(f.Size) / fupe)
+		if currentX > maxX {
+			maxX = currentX
+		}
+
+		switch {
+		case char == 'i':
+			fallthrough
+		case char == 'j':
+			fallthrough
+		case char == 'e':
+			fallthrough
+		case char == 'Q':
+			fmt.Println(string(char), float32(vm.TopSideBearing), float32(g.Bounds.Min.X), float32(g.Bounds.Min.Y), float32(g.Bounds.Max.X), float32(g.Bounds.Max.Y), float32(g.Bounds.Max.Y-g.Bounds.Min.Y))
+		}
+
+		//yB := vm.TopSideBearing
+		yB := g.Bounds.Max.Y - g.Bounds.Min.Y
+		if yB > maxYBearing {
+			maxYBearing = yB
+		}
+		atlas.Height[char] = float32(yB * fixed.Int26_6(f.Size) / fupe)
+
+		if int(int26Width*fixed.Int26_6(f.Size)/fupe) > 1024 {
+			// Now let's draw these chars!
+			subimg := f.RenderNRGBA(subString)
+			// TODO: optimize this!
+			draw.Draw(nrgba, image.Rect(0, drawCurY, subimg.Bounds().Max.X, drawCurY+subimg.Bounds().Max.Y), subimg, image.ZP, draw.Src)
+			drawCurY += subimg.Bounds().Max.Y
+
+			/*
+				for subI := lastI; subI <= i; subI++ {
+					atlas.Height[rune(subI)] = float32(subimg.Bounds().Max.Y)
+				}
+			*/
+			fmt.Sprint(lastI)
+
+			int26Height += maxYBearing
+			totalHeight += maxYBearing
+			maxYBearing = fixed.Int26_6(0)
+
+			subString = ""
+			int26Width = 0
+			currentX = 0
+			lastI = i + 1
+		}
+	}
+
+	// TODO: draw last line?!
+
+	atlas.TotalWidth = maxX
+	atlas.TotalHeight = float32(drawCurY)
+
+	// Create texture
+	actual := image.NewNRGBA(image.Rect(0, 0, int(atlas.TotalWidth), int(atlas.TotalHeight)))
+	draw.Draw(actual, actual.Bounds(), nrgba, image.ZP, draw.Src)
+
+	file, err := os.OpenFile("/tmp/debug.webp", os.O_CREATE|os.O_RDWR|os.O_TRUNC, os.FileMode(0777))
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+	err = webp.Encode(file, actual, &webp.Options{Quality: 100})
+	if err != nil {
+		panic(err)
+	}
+
+	imObj := NewImageObject(actual)
+	atlas.Texture = NewTextureSingle(imObj).id
+	return atlas
+}
+
+// A FontAtlas is a representation of some of the Font characters, as an image
+type FontAtlas struct {
+	Texture     *gl.Texture
+	XLocation   []float32
+	YLocation   []float32
+	Width       []float32
+	Height      []float32
+	TotalWidth  float32
+	TotalHeight float32
+}
+
+// Text represents a string drawn on the screen
+type Text struct {
+	Font *Font
+	Text string
+}
+
+func (Text) Texture() *gl.Texture                       { return nil }
+func (Text) Width() float32                             { return 0 }
+func (Text) Height() float32                            { return 0 }
+func (Text) View() (float32, float32, float32, float32) { return 0, 0, 1, 1 }
+func (Text) Close()                                     {}
--- demos/text/text.go
+++ demos/text/text.go
@@ -11,25 +11,80 @@ import (
 type DefaultScene struct{}
 
 var (
+	zoomSpeed   float32 = -0.125
+	scrollSpeed float32 = 700
+
 	worldWidth  int = 800
 	worldHeight int = 800
 )
 
-func (*DefaultScene) Preload() {}
+type MyLabel struct {
+	ecs.BasicEntity
+	common.RenderComponent
+	common.SpaceComponent
+}
+
+func (*DefaultScene) Preload() {
+	err := engo.Files.LoadMany(
+		"Roboto-Regular.ttf",
+	)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // Setup is called before the main loop is started
 func (*DefaultScene) Setup(w *ecs.World) {
-	common.SetBackground(color.RGBA{55, 55, 55, 255})
+	common.SetBackground(color.White)
 	w.AddSystem(&common.RenderSystem{})
+
+	// Adding KeyboardScroller so we can actually see the difference between background and HUD when scrolling
+	w.AddSystem(common.NewKeyboardScroller(scrollSpeed, engo.DefaultHorizontalAxis, engo.DefaultVerticalAxis))
+	w.AddSystem(&common.MouseZoomer{zoomSpeed})
+
+	fnt := &common.Font{
+		URL:  "Roboto-Regular.ttf",
+		FG:   color.Black,
+		Size: 64,
+	}
+	err := fnt.CreatePreloaded()
+	if err != nil {
+		panic(err)
+	}
+
+	label1 := MyLabel{BasicEntity: ecs.NewBasic()}
+	label1.RenderComponent.Drawable = common.Text{
+		Font: fnt,
+		Text: "Hello world !",
+	}
+	label1.SpaceComponent.Position = engo.Point{500, 500}
+	label1.RenderComponent.SetShader(common.TextHUDShader)
+
+	label2 := MyLabel{BasicEntity: ecs.NewBasic()}
+	label2.RenderComponent.Drawable = common.Text{
+		Font: fnt,
+		Text: "This is \nmultilined Q :-), very nice!",
+	}
+	label2.SpaceComponent.Position = engo.Point{300, 300}
+	label2.RenderComponent.SetShader(common.TextShader)
+
+	for _, system := range w.Systems() {
+		switch sys := system.(type) {
+		case *common.RenderSystem:
+			sys.Add(&label1.BasicEntity, &label1.RenderComponent, &label1.SpaceComponent)
+			sys.Add(&label2.BasicEntity, &label2.RenderComponent, &label2.SpaceComponent)
+		}
+	}
 }
 
 func (*DefaultScene) Type() string { return "Game" }
 
 func main() {
 	opts := engo.RunOptions{
-		Title:  "Text Demo",
-		Width:  worldWidth,
-		Height: worldHeight,
+		Title:          "Text Demo",
+		Width:          worldWidth,
+		Height:         worldHeight,
+		StandardInputs: true,
 	}
 	engo.Run(opts, &DefaultScene{})
 }
