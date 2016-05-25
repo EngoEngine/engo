707a2db0dc1ef78f815f8a032c427b9ba51fb838
Ousmane Traore <hello@ousmane.me>
2016-05-25 13:52:26 -0400

    Add tests

---
+++ engo_test.go
@@ -0,0 +1,47 @@
+package engo
+
+import (
+    "testing"
+    
+    "github.com/github.com/stretchr/testify/assert"
+)
+
+func TestSetScaleOnResize(t *testing.T) {
+    SetScaleOnResize(true)
+    assert.Equal(t, opts.ScaleOnResize, true)
+    assert.Equal(t, ScaleOnResize(), true)
+    SetScaleOnResize(false)
+    assert.Equal(t, opts.ScaleOnResize, false)
+    assert.Equal(t, ScaleOnResize(), false)
+}
+
+func TestSetOverrideCloseAction(t *testing.T) {
+    SetOverrideCloseAction(true)
+    assert.Equal(t, opts.OverrideCloseAction, true)
+    SetOverrideCloseAction(false)
+    assert.Equal(t, opts.OverrideCloseAction, false)
+}
+
+func TestSetFPSLimit(t *testing.T) {
+    err := SetFPSLimit(60)
+    assert.Equal(t, err, nil)
+    assert.Equal(t, opts.FPSLimit, 60)
+    err = SetFPSLimit(-1)
+    assert.NotEqual(t, err, nil)
+}
+
+func TestHeadless(t *testing.T) {
+    SetHeadless(false)
+    assert.Equal(t, opts.HeadlessMode, false)
+    assert.Equal(t, Headless(), false)
+    SetHeadless(true)
+    assert.Equal(t, opts.HeadlessMode, true)
+    assert.Equal(t, Headless)
+}
+
+
+func TestExit(t *testing.T) {
+    assert.Equal(t, closeGame, false)
+    Exit()
+    assert.Equal(t, closeGame, true)
+}
