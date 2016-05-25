e0af152019222eb8431bae0c1eb6c980142b02db
Ousmane Traore <hello@ousmane.me>
2016-05-25 14:47:09 -0400

    Fix

--- engo_glfw_test.go
+++ engo_glfw_test.go
@@ -3,27 +3,27 @@
 package engo
 
 import (
-    "testing"
-    
-    "github.com/github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/github.com/stretchr/testify/assert"
 )
 
 func TestGameSize(t *testing.T) {
-    assert.Equal(t, GameWidth(), gameWidth)
-    assert.Equal(t, GameHeight(), gameHeight)    
+	assert.Equal(t, GameWidth(), gameWidth)
+	assert.Equal(t, GameHeight(), gameHeight)
 }
 
 func TestCursorPos(t *testing.T) {
-    assert.Equal(t, CursorPos(), window.GetCursorPos())
+	assert.Equal(t, CursorPos(), window.GetCursorPos())
 }
 
 func TestWindowSize(t *testing.T) {
-    assert.Equal(t, WindowSize(), window.GetSize())
-    assert.Equal(t, WindowWidth(), windowWidth)
-    assert.Equal(t, WindowHeight(), windowHeight)
+	assert.Equal(t, WindowSize(), window.GetSize())
+	assert.Equal(t, WindowWidth(), windowWidth)
+	assert.Equal(t, WindowHeight(), windowHeight)
 }
 
 func TestCanvasSize(t *testing.T) {
-    assert.Equal(t, CanvasWidth(), canvasWidth)
-    assert.Equal(t, CanvasHeight(), canvasHeight)
+	assert.Equal(t, CanvasWidth(), canvasWidth)
+	assert.Equal(t, CanvasHeight(), canvasHeight)
 }
--- engo_test.go
+++ engo_test.go
@@ -1,47 +1,46 @@
 package engo
 
 import (
-    "testing"
-    
-    "github.com/github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/github.com/stretchr/testify/assert"
 )
 
 func TestSetScaleOnResize(t *testing.T) {
-    SetScaleOnResize(true)
-    assert.True(t, opts.ScaleOnResize)
-    assert.True(t, ScaleOnResize())
-    SetScaleOnResize(false)
-    assert.False(t, opts.ScaleOnResize)
-    assert.False(t, ScaleOnResize())
+	SetScaleOnResize(true)
+	assert.True(t, opts.ScaleOnResize)
+	assert.True(t, ScaleOnResize())
+	SetScaleOnResize(false)
+	assert.False(t, opts.ScaleOnResize)
+	assert.False(t, ScaleOnResize())
 }
 
 func TestSetOverrideCloseAction(t *testing.T) {
-    SetOverrideCloseAction(true)
-    assert.True(t, opts.OverrideCloseAction)
-    SetOverrideCloseAction(false)
-    assert.True(t, opts.OverrideCloseAction)
+	SetOverrideCloseAction(true)
+	assert.True(t, opts.OverrideCloseAction)
+	SetOverrideCloseAction(false)
+	assert.True(t, opts.OverrideCloseAction)
 }
 
 func TestSetFPSLimit(t *testing.T) {
-    err := SetFPSLimit(60)
-    assert.Nil(t, err)
-    assert.Equal(t, opts.FPSLimit, 60)
-    err = SetFPSLimit(-1)
-    assert.NotNil(t, err)
+	err := SetFPSLimit(60)
+	assert.Nil(t, err)
+	assert.Equal(t, opts.FPSLimit, 60)
+	err = SetFPSLimit(-1)
+	assert.NotNil(t, err)
 }
 
 func TestHeadless(t *testing.T) {
-    SetHeadless(false)
-    assert.False(t, opts.HeadlessMode)
-    assert.False(t, Headless())
-    SetHeadless(true)
-    assert.True(t, opts.HeadlessMode, )
-    assert.Equal(t, Headless)
+	SetHeadless(false)
+	assert.False(t, opts.HeadlessMode)
+	assert.False(t, Headless())
+	SetHeadless(true)
+	assert.True(t, opts.HeadlessMode)
+	assert.Equal(t, Headless)
 }
 
-
 func TestExit(t *testing.T) {
-    assert.False(t, closeGame)
-    Exit()
-    assert.True(t, closeGame)
+	assert.False(t, closeGame)
+	Exit()
+	assert.True(t, closeGame)
 }
