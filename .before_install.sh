5608c22e5316c8ef2d3286823c34fc5fe6fd4fd7
Jerry Caligiure <caligiure.ja@gmail.com>
2018-03-06 17:32:19 +0000

    yet another sring comparison attempt

--- .before_install.sh
+++ .before_install.sh
@@ -4,18 +4,18 @@ go get golang.org/x/tools/cmd/cover
 go get github.com/mdempsky/unconvert
 go get github.com/mattn/goveralls
 
-if [$TEST_TYPE=js_test] || [$TEST_TYPE=js_build]
+if [ "$TEST_TYPE" == "js_test" ] || [ "$TEST_TYPE" == "js_build" ]
 then
     go get github.com/gopherjs/gopherjs
     gopherjs get "honnef.co/go/js/dom"
     gopherjs get "honnef.co/go/js/xhr"
-elif [$TEST_TYPE=android_test] || [$TEST_TYPE=android_build]
+elif [ "$TEST_TYPE" == "android_test" ] || [ "$TEST_TYPE" == "android_build" ]
 then
     go get golang.org/x/mobile/cmd/gomobile
     gomobile init
     git clone https://github.com/Noofbiz/android-ndk.git $HOME/android-ndk-root
     printf "$HOME/android-ndk-root" > $GOPATH/pkg/gomobile/android_ndk_root
-elif [$TEST_TYPE=linux_test] || [$TEST_TYPE=linux_build]
+elif [ "$TEST_TYPE" == "linux_test" ] || [ "$TEST_TYPE" == "linux_build" ]
 then
     echo "nothing more to add for linux"
 else
