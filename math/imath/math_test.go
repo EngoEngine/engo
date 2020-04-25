07a2d09cba810cf682334fac5c207b1fda908353
Ilyar <ilyar.software@gmail.com>
2020-04-25 06:27:21 +0000

    fix use assert instead of log

--- math/imath/math_test.go
+++ math/imath/math_test.go
@@ -1,12 +1,16 @@
-package imath
+package imath_test
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/EngoEngine/engo/math/imath"
 )
 
 func TestSqrt(t *testing.T) {
-	t.Log(Sqrt(25))
-	t.Log(Sqrt(24))
-	t.Log(Sqrt(400))
-	t.Log(Sqrt(399))
+	assert.Equal(t, 5, imath.Sqrt(25))
+	assert.Equal(t, 4, imath.Sqrt(24))
+	assert.Equal(t, 20, imath.Sqrt(400))
+	assert.Equal(t, 19, imath.Sqrt(399))
 }
